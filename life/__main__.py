143040eeadfb6f8ceea5ee3ad75f1d55cd468c42
Artem Mavrin <amavrin@ucsd.edu>
2018-10-11 22:46:20 -0700

    Refactored repetetive code

--- life/__main__.py
+++ life/__main__.py
@@ -8,10 +8,10 @@ import itertools
 from .life import Life, Geometry, GEOMETRIES
 
 # Dead cells are marked by the space character: ' '
-_CH_DEAD = chr(32)
+_CH_DEAD = " "
 
 # Living cells are marked by the character '@'
-_CH_ALIVE = chr(64)
+_CH_ALIVE = "@"
 
 # Program name
 _PROGRAM = "python -m life"
@@ -46,6 +46,72 @@ def draw_board(screen, life):
     """Draw the entire game of life board."""
     for row, col in itertools.product(range(life.n_rows), range(life.n_cols)):
         draw_cell(screen, life, row, col)
+
+
+def move_cursor_up(life, row, col):
+    """Move the cursor up if possible."""
+    if row > 0:
+        row -= 1
+    else:
+        if life.geometry in (Geometry.TORUS, Geometry.KLEIN_BOTTLE):
+            row = life.n_rows - 1
+        elif life.geometry == Geometry.PROJECTIVE_PLANE:
+            row = life.n_rows - 1
+            col = life.n_cols - col - 1
+        else:
+            curses.flash()
+
+    return row, col
+
+
+def move_cursor_down(life, row, col):
+    """Move the cursor down if possible."""
+    if row < life.n_rows - 1:
+        row += 1
+    else:
+        if life.geometry in (Geometry.TORUS, Geometry.KLEIN_BOTTLE):
+            row = 0
+        elif life.geometry == Geometry.PROJECTIVE_PLANE:
+            row = 0
+            col = life.n_cols - col - 1
+        else:
+            curses.flash()
+
+    return row, col
+
+
+def move_cursor_left(life, row, col):
+    """Move the cursor left if possible."""
+    if col > 0:
+        col -= 1
+    else:
+        if life.geometry in (Geometry.CYLINDER, Geometry.TORUS):
+            col = life.n_cols - 1
+        elif life.geometry in (Geometry.MOBIUS_STRIP, Geometry.KLEIN_BOTTLE,
+                               Geometry.PROJECTIVE_PLANE):
+            col = life.n_cols - 1
+            row = life.n_rows - row - 1
+        else:
+            curses.flash()
+
+    return row, col
+
+
+def move_cursor_right(life, row, col):
+    """Move the cursor right if possible."""
+    if col < life.n_cols - 1:
+        col += 1
+    else:
+        if life.geometry in (Geometry.CYLINDER, Geometry.TORUS):
+            col = 0
+        elif life.geometry in (Geometry.MOBIUS_STRIP, Geometry.KLEIN_BOTTLE,
+                               Geometry.PROJECTIVE_PLANE):
+            col = 0
+            row = life.n_rows - row - 1
+        else:
+            curses.flash()
+
+    return row, col
 
 
 def main(stdscr, *, geometry, prob, seed, delay, draw):
@@ -102,54 +168,16 @@ def main(stdscr, *, geometry, prob, seed, delay, draw):
                 draw_cell(stdscr, life, cursor_row, cursor_col)
             elif c == curses.KEY_UP:
                 # Move the cursor up if possible
-                if cursor_row > 0:
-                    cursor_row -= 1
-                else:
-                    if life.geometry in (Geometry.TORUS, Geometry.KLEIN_BOTTLE):
-                        cursor_row = life.n_rows - 1
-                    elif life.geometry == Geometry.PROJECTIVE_PLANE:
-                        cursor_row = life.n_rows - 1
-                        cursor_col = life.n_cols - cursor_col - 1
-                    else:
-                        curses.flash()
+                cursor_row, cursor_col = move_cursor_up(life, cursor_row, cursor_col)
             elif c == curses.KEY_DOWN:
                 # Move the cursor down if possible
-                if cursor_row < life.n_rows - 1:
-                    cursor_row += 1
-                else:
-                    if life.geometry in (Geometry.TORUS, Geometry.KLEIN_BOTTLE):
-                        cursor_row = 0
-                    elif life.geometry == Geometry.PROJECTIVE_PLANE:
-                        cursor_row = 0
-                        cursor_col = life.n_cols - cursor_col - 1
-                    else:
-                        curses.flash()
+                cursor_row, cursor_col = move_cursor_down(life, cursor_row, cursor_col)
             elif c == curses.KEY_LEFT:
                 # Move the cursor left if possible
-                if cursor_col > 0:
-                    cursor_col -= 1
-                else:
-                    if life.geometry in (Geometry.CYLINDER, Geometry.TORUS):
-                        cursor_col = life.n_cols - 1
-                    elif life.geometry in (Geometry.MOBIUS_STRIP, Geometry.KLEIN_BOTTLE,
-                                           Geometry.PROJECTIVE_PLANE):
-                        cursor_col = life.n_cols - 1
-                        cursor_row = life.n_rows - cursor_row - 1
-                    else:
-                        curses.flash()
+                cursor_row, cursor_col = move_cursor_left(life, cursor_row, cursor_col)
             elif c == curses.KEY_RIGHT:
                 # Move the cursor right if possible
-                if cursor_col < life.n_cols - 1:
-                    cursor_col += 1
-                else:
-                    if life.geometry in (Geometry.CYLINDER, Geometry.TORUS):
-                        cursor_col = 0
-                    elif life.geometry in (Geometry.MOBIUS_STRIP, Geometry.KLEIN_BOTTLE,
-                                           Geometry.PROJECTIVE_PLANE):
-                        cursor_col = 0
-                        cursor_row = life.n_rows - cursor_row - 1
-                    else:
-                        curses.flash()
+                cursor_row, cursor_col = move_cursor_right(life, cursor_row, cursor_col)
             elif curses.ascii.isalpha(c) and chr(c) in "rR":
                 # If the user presses the 'R' key, randomize the board
                 life.randomize()
